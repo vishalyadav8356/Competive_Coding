f0af25dd922431c90844c0e87f34ccced20dfc75
Vishal Yadav <119524735+vishalyadav8356@users.noreply.github.com>
2025-07-01 00:18:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (78.17%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
+    int go(int num){
+        if(num == 0) return 0;
+        if(num%2 == 0) return 1+go(num/2);
+        return 1+go(num-1);
+    } 
     int numberOfSteps(int num) {
-        int step = 0;
-        while(num>0){
-        if(num%2 == 0){
-            num/=2;
-        }
-        else{
-            num-=1; 
-        }
-        step++;
-        }
-        return step;
+        return go(num);
+        // int step = 0;
+        // while(num>0){
+        // if(num%2 == 0){
+        //     num/=2;
+        // }
+        // else{
+        //     num-=1; 
+        // }
+        // step++;
+        // }
+        // return step;
+
+
+        //By recusrion
+        
     }
 };
